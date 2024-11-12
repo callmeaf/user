22b4992dbf49ae77809ecef5a694dd6438bc5c1f
callmeaf <alikhoshkar2000@gmail.com>
2024-11-12 12:43:13 +0330

    Change path of HasRoles Trait

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,21 +2,21 @@
 
 namespace Callmeaf\User\Models;
 
+use Callmeaf\Auth\Notifications\V1\VerifyEmail;
+use Callmeaf\Base\Contracts\HasEnum;
 use Callmeaf\Base\Contracts\HasResponseTitles;
 use Callmeaf\Base\Enums\ResponseTitle;
 use Callmeaf\Base\Traits\HasCarts;
-use Callmeaf\Base\Traits\HasMediaMethod;
-use Callmeaf\Base\Traits\Localeable;
-use Callmeaf\Base\Traits\Metaable;
-use Callmeaf\Permission\Traits\HasRoles;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Callmeaf\Auth\Notifications\V1\VerifyEmail;
-use Callmeaf\Base\Contracts\HasEnum;
 use Callmeaf\Base\Traits\HasDate;
+use Callmeaf\Base\Traits\HasMediaMethod;
+use Callmeaf\Base\Traits\HasRoles;
 use Callmeaf\Base\Traits\HasStatus;
 use Callmeaf\Base\Traits\HasType;
+use Callmeaf\Base\Traits\Localeable;
+use Callmeaf\Base\Traits\Metaable;
 use Callmeaf\User\Enums\UserStatus;
 use Callmeaf\User\Enums\UserType;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
